bd88a7f2e68208b4ab656008000f62de99eb940f
Pavel Zhukov <pzhukov@redhat.com>
2013-04-25 09:13:31 +0200

    Add read_config call

--- src/main.adb
+++ src/main.adb
@@ -3,10 +3,16 @@ with Ada.Text_IO; use Ada.Text_IO;
 with GNAT.OS_Lib; use GNAT.OS_Lib;
 with ARPM_Walkers; use ARPM_Walkers;
 with ARPM_Processors; use ARPM_Processors;
+with ARPM_C_Bridge;
+
 procedure Main is 
     threads : Positive := 8;
     pool : array (1..threads) of ARPM_Processor;
+    ERROR_RPM_CONFIG : exception;
 begin
+    if Integer(ARPM_C_Bridge.Read_Config) /= 0 then
+        raise ERROR_RPM_CONFIG;
+    end if;
     if Ada.Command_Line.Argument_Count /= 1 then
         Put_Line("Directory name is not specified");
         OS_Exit(1);
