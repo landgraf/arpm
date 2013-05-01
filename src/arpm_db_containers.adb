678e24b71307bac4981d4a3a10f24c3087c0b47c
Pavel Zhukov <pavel@zhukoff.net>
2013-05-01 22:21:44 +0200

    Optimize Q.Prepare

--- src/arpm_db_containers.adb
+++ src/arpm_db_containers.adb
@@ -20,15 +20,16 @@ package body ARPM_DB_Containers is
     procedure Save_Provides(RPM : in ARPM_RPM_Access; DB : in ARPM_DB_Container_Access) is 
         Q : SQL.Queries.SQL_Query := DB.Handler.Query;
     begin
+        Q.Prepare (+"INSERT INTO  provides ( name, version, release, provideKey) VALUES ( :name, :version, :release, :pkey)");
         for I in 1..Length(RPM.Provides) loop
             if not ARPM_Files_Handlers.DB_Keys.Has_Provide_Key(RPM.Provides.Element(I)) then 
                 ARPM_Files_Handlers.DB_Keys.Add_Provide_Key (RPM.Provides.Element(I));
-                Q.Prepare (+"INSERT INTO  provides ( name, version, release, provideKey) VALUES ( :name, :version, :release, :pkey)");
                 Q.Bind_Value (+":name", League.Holders.To_Holder(RPM.Provides.Element(I)));
                 Q.Bind_Value (+":version", League.Holders.To_Holder(To_Universal_string("Fixme")));
                 Q.Bind_Value (+":release", League.Holders.To_Holder(To_Universal_string("Fixme")));
                 Q.Bind_Value (+":pkey", League.Holders.To_Holder (SHA256(RPM.Provides.Element(I))));
                 Q.Execute;
+                Q.Finish;
             end if;
         end loop;
     exception
@@ -40,15 +41,16 @@ package body ARPM_DB_Containers is
     procedure Save_requires(RPM : in ARPM_RPM_Access; DB : in ARPM_DB_Container_Access) is 
         Q : SQL.Queries.SQL_Query := DB.Handler.Query;
     begin
+        Q.Prepare (+"INSERT INTO  requires ( name, version, release, requireKey) VALUES ( :name, :version, :release, :pkey)");
         for I in 1..Length(RPM.requires) loop
             if not ARPM_Files_Handlers.DB_Keys.Has_require_Key(RPM.requires.Element(I)) then 
                 ARPM_Files_Handlers.DB_Keys.Add_Require_Key (RPM.requires.Element(I));
-                Q.Prepare (+"INSERT INTO  requires ( name, version, release, requireKey) VALUES ( :name, :version, :release, :pkey)");
                 Q.Bind_Value (+":name", League.Holders.To_Holder(RPM.requires.Element(I)));
                 Q.Bind_Value (+":version", League.Holders.To_Holder(To_Universal_string("Fixme")));
                 Q.Bind_Value (+":release", League.Holders.To_Holder(To_Universal_string("Fixme")));
                 Q.Bind_Value (+":pkey", League.Holders.To_Holder (SHA256(RPM.requires.Element(I))));
                 Q.Execute;
+                Q.Finish;
             end if;
         end loop;
     exception
