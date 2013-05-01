9752082c7fea324d6b1ac8dedabff3755b64a6a2
Pavel Zhukov <pavel@zhukoff.net>
2013-05-01 22:05:56 +0200

    Replace incremented integers with sha256

--- sql/arpm.sql
+++ sql/arpm.sql
@@ -4,16 +4,16 @@ PRAGMA default_cache_size = 4000;
 PRAGMA synchronous = OFF;
 PRAGMA temp_store = MEMORY;
 PRAGMA journal_mode = OFF;
-CREATE TABLE conflicts (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgKey INTEGER );
+CREATE TABLE conflicts (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgKey CHAR(65));
 CREATE TABLE db_info (dbversion INTEGER, checksum TEXT);
-CREATE TABLE files (  name TEXT,  type TEXT,  pkgKey INTEGER);
-CREATE TABLE obsoletes (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgKey INTEGER );
-CREATE TABLE packages (  pkgKey INTEGER PRIMARY KEY,  pkgId TEXT,  name TEXT,  arch TEXT,  version TEXT,  epoch TEXT,  release TEXT,  summary TEXT,  description TEXT,  url TEXT,  time_file INTEGER,  time_build INTEGER,  rpm_license TEXT,  rpm_vendor TEXT,  rpm_group TEXT,  rpm_buildhost TEXT,  rpm_sourcerpm TEXT,  rpm_header_start INTEGER,  rpm_header_end INTEGER,  rpm_packager TEXT,  size_package INTEGER,  size_installed INTEGER,  size_archive INTEGER,  location_href TEXT,  location_base TEXT,  checksum_type TEXT);
-CREATE TABLE provides (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgKey INTEGER , provideKey INTEGER);
-CREATE TABLE packages_provides ( pkgKey INTEGER, provideKey INTEGER, FOREIGN KEY(pkgKey) REFERENCES packages(pkgKey) ON DELETE CASCADE, 
+CREATE TABLE files (  name TEXT,  type TEXT,  pkgKey CHAR(65));
+CREATE TABLE obsoletes (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgKey CHAR(65) );
+CREATE TABLE packages (  pkgKey CHAR(65) PRIMARY KEY,  pkgId TEXT,  name TEXT,  arch TEXT,  version TEXT,  epoch TEXT,  release TEXT,  summary TEXT,  description TEXT,  url TEXT,  time_file INTEGER,  time_build INTEGER,  rpm_license TEXT,  rpm_vendor TEXT,  rpm_group TEXT,  rpm_buildhost TEXT,  rpm_sourcerpm TEXT,  rpm_header_start INTEGER,  rpm_header_end INTEGER,  rpm_packager TEXT,  size_package INTEGER,  size_installed INTEGER,  size_archive INTEGER,  location_href TEXT,  location_base TEXT,  checksum_type TEXT);
+CREATE TABLE provides (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgKey CHAR(65) , provideKey CHAR(65));
+CREATE TABLE packages_provides ( pkgKey CHAR(65), provideKey CHAR(65), FOREIGN KEY(pkgKey) REFERENCES packages(pkgKey) ON DELETE CASCADE, 
     FOREIGN KEY(provideKey) REFERENCES provides(providesKey) ON DELETE CASCADE);
-CREATE TABLE requires (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgKey INTEGER , pre BOOLEAN DEFAULT FALSE, requireKey INTEGER);
-CREATE TABLE packages_requires ( pkgKey INTEGER, requireKey INTEGER, FOREIGN KEY(pkgKey) REFERENCES packages(pkgKey) ON DELETE CASCADE, 
+CREATE TABLE requires (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgkey CHAR(65) , pre BOOLEAN DEFAULT FALSE, requirekey CHAR(65));
+CREATE TABLE packages_requires ( pkgkey CHAR(65), requirekey CHAR(65), FOREIGN KEY(pkgKey) REFERENCES packages(pkgKey) ON DELETE CASCADE, 
     FOREIGN KEY(requireKey) REFERENCES requires(requiresKey) ON DELETE CASCADE);
 CREATE INDEX filenames ON files (name);
 CREATE INDEX packageId ON packages (pkgId);
