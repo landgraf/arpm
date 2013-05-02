a20642e9c4df3e46e127351005e32af46e38aad0
Pavel Zhukov <pavel@zhukoff.net>
2013-05-02 21:19:20 +0200

    Imrove argument parser

--- src/createrepo.adb
+++ src/createrepo.adb
@@ -7,18 +7,23 @@ with ARPM_Files_Handlers;
 with ARPM_C_Bridge;
 
 procedure Createrepo is 
+    function Get_Dir return String is 
+    begin
+        return Argument(1);
+    exception
+        when CONSTRAINT_ERROR => 
+            Put_Line("Please specify directory name");
+            OS_Exit(1);
+    end Get_Dir;
     threads : Positive := 8;
     pool : array (1..threads) of ARPM_Processor;
     ERROR_RPM_CONFIG : exception;
+    Dir : constant String := Get_Dir;
 begin
     if Integer(ARPM_C_Bridge.Read_Config) /= 0 then
         raise ERROR_RPM_CONFIG;
     end if;
-    if Ada.Command_Line.Argument_Count /= 1 then
-        Put_Line("Directory name is not specified");
-        OS_Exit(1);
-    end if;
-    Start(Argument(1));
+    Start(Dir);
     ARPM_Files_Handlers.Workers.Is_Empty;
     ARPM_Files_Handlers.DB.Free;
 end Createrepo;
