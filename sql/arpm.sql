4019b2769334cfd5d6643e96cd0cc123b5474e6f
Pavel Zhukov <pavel@zhukoff.net>
2013-05-04 22:00:17 +0200

    Remove foreign key

--- sql/arpm.sql
+++ sql/arpm.sql
@@ -9,12 +9,10 @@ CREATE TABLE db_info (dbversion INTEGER, checksum TEXT);
 CREATE TABLE files (  name TEXT,  type TEXT,  pkgKey CHAR(65));
 CREATE TABLE obsoletes (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgKey CHAR(65) );
 CREATE TABLE packages (  pkgKey CHAR(65) PRIMARY KEY,  pkgId TEXT,  name TEXT,  arch TEXT,  version TEXT,  epoch TEXT,  release TEXT,  summary TEXT,  description TEXT,  url TEXT,  time_file INTEGER,  time_build INTEGER,  rpm_license TEXT,  rpm_vendor TEXT,  rpm_group TEXT,  rpm_buildhost TEXT,  rpm_sourcerpm TEXT,  rpm_header_start INTEGER,  rpm_header_end INTEGER,  rpm_packager TEXT,  size_package INTEGER,  size_installed INTEGER,  size_archive INTEGER,  location_href TEXT,  location_base TEXT,  checksum_type TEXT);
-CREATE TABLE provides (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgKey CHAR(65) , provideKey CHAR(65));
-CREATE TABLE packages_provides ( pkgKey CHAR(65), provideKey CHAR(65), FOREIGN KEY(pkgKey) REFERENCES packages(pkgKey) ON DELETE CASCADE, 
-    FOREIGN KEY(provideKey) REFERENCES provides(providesKey) ON DELETE CASCADE);
-CREATE TABLE requires (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgkey CHAR(65) , pre BOOLEAN DEFAULT FALSE, requirekey CHAR(65));
-CREATE TABLE packages_requires ( pkgkey CHAR(65), requirekey CHAR(65), FOREIGN KEY(pkgKey) REFERENCES packages(pkgKey) ON DELETE CASCADE, 
-    FOREIGN KEY(requireKey) REFERENCES requires(requiresKey) ON DELETE CASCADE);
+CREATE TABLE provides (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgKey CHAR(65) , provideKey CHAR(65) PRIMARY KEY);
+CREATE TABLE packages_provides ( pkgKey CHAR(65), provideKey CHAR(65));
+CREATE TABLE requires (  name TEXT,  flags TEXT,  epoch TEXT,  version TEXT,  release TEXT,  pkgkey CHAR(65) , pre BOOLEAN DEFAULT FALSE, requirekey CHAR(65) PRIMARY KEY);
+CREATE TABLE packages_requires ( pkgkey CHAR(65), requirekey CHAR(65) );
 CREATE INDEX filenames ON files (name);
 CREATE INDEX packageId ON packages (pkgId);
 CREATE INDEX packagename ON packages (name);
